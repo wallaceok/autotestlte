2f606bfb676b035b666d7408a6e455cda7a93558
李海民 <lihaimin126@126.com>
2019-05-21 16:20:10 +0800

    添加产品数统计

--- product/proviews.py
+++ product/proviews.py
@@ -18,6 +18,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def product_manage(request):
     username = request.session.get('user', '')
     product_list = Product.objects.all()
+    product_count = Product.objects.all().count()  # 统计产品数
     paginator = Paginator(product_list, 8)  # 生成 paginator 对象，设置每页显示 8 条记录
     page = request.GET.get('page', 1)  # 获取当前的页码数,默认为第 1 页
     currentPage = int(page)  # 把获取的当前页码数转换成整数类型
@@ -27,7 +28,7 @@ def product_manage(request):
         product_list = paginator.page(1)  # 如果输入的页数不是整数，则显示第 1 页内容
     except EmptyPage:
         product_list = paginator.page(paginator.num_pages)  # 如果输入的页数不在系统的页数# 中，则显示最后一页内容
-    return render(request, "product_manage.html", {"user": username, "products": product_list})
+    return render(request, "product_manage.html", {"user": username, "products": product_list,"productcounts":product_count})  #把值赋给 productcounts 变量
 
 # 搜索功能
 @login_required
